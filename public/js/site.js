07d4df1e64e130bd6253cffa46a6571b15135458
Jue Yang <jueyang22@gmail.com>
2015-08-17 01:10:42 -0700

    change baselayer tiles to https

--- public/js/site.js
+++ public/js/site.js
@@ -44,7 +44,7 @@ Scavenger.prototype.createMap = function(team){
 		// maxZoom:14
 	});
 
-	L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+	L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
 	}).addTo(map);
 
