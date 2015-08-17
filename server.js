eadd36d69dc7ca1515b36142a24c0c4f2ae668a8
Jue Yang <jueyang22@gmail.com>
2015-08-16 20:01:58 -0700

    create a new cartoClient per POST request, see issue 31 in cartodb-nodejs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var ig = require('instagram-node').instagram();
 var _ = require('lodash');
-var cartoClient = require('./models/database.js');
+var CartoDB = require('cartodb');
 var token = require('./token.js');
 
 // Instagram setup
@@ -65,9 +65,56 @@ app.post('/team/:team',function(req,res){
 
 			rows.push(rowValue);
 		});
+
+		// using single client for the app creates multiple connections
+		// see https://github.com/Vizzuality/cartodb-nodejs/issues/31
+		// temp solution: create new instance of cartoClient within the route
+
+		// CartoDB setup
+		var cartoClient = new CartoDB({
+			user:token.cartodb_user,
+			api_key:token.cartodb_api_key
+		});
+
+		// add function to CartoClient
+		cartoClient.acceptString = function(valueString){
+			cartoClient.on('connect',function(){
+				console.log('cartoDB connected once');
+
+				// scavenger_carto (the_geom,ig_created_time, ig_id, ig_link, ig_thumbnail, team_name)
+
+				var queryBlock ="WITH n(the_geom, ig_created_time, ig_id, ig_link, ig_thumbnail, team_name) AS (" +
+								 	"VALUES " + valueString +
+								")," +
+								"upsert AS (" +
+									"UPDATE {table} o " +
+									"SET the_geom = n.the_geom " + // only update geo location now, all others are constant
+									"FROM n WHERE o.ig_id = n.ig_id " +
+									"RETURNING o.ig_id" +
+								")" +
+								"INSERT INTO {table} (the_geom, ig_created_time, ig_id, ig_link, ig_thumbnail, team_name) " +
+								"SELECT * FROM n " +
+								"WHERE n.ig_id NOT IN (" +
+									"SELECT ig_id FROM upsert" +
+								")";
+
+				cartoClient.query(queryBlock,{table:token.cartodb_table},function(err,data){
+					console.log(data);
+				})
+
+			});
+		}
+
 		// Stringify values to be updated or inserted to CartoDB
 		cartoClient.acceptString(_(rows).toString());
+		cartoClient.connect();
 	};
+
+	// TODO pagination
+	ig.tag_media_recent(teamName, hdl);
+
+	// passing back the team name
+	res.status(200).json(req.body);
 });
 
 app.listen(process.env.PORT || 8080);
